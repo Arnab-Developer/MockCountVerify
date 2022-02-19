177febdb7e58062e21ca9e418028aa3cba5b9f14
Arnab Roy Chowdhury <arnab.roy.chowdhury.developer@gmail.com>
2022-02-19 16:02:45 +0530

    Add test

---
+++ MockCountVerify.ConsoleAppTest/WorkerTest.cs
@@ -0,0 +1,24 @@
+using MockCountVerify.ConsoleApp;
+using Moq;
+using Xunit;
+
+namespace MockCountVerify.ConsoleAppTest;
+
+public class WorkerTest
+{
+    [Fact]
+    public void VerifyDoWorkWorksProperly()
+    {
+        Mock<ILogger> loggerMock = new();
+        Worker worker = new(loggerMock.Object);
+        
+        loggerMock
+            .Setup(logger => logger.Log(It.IsAny<string>()));
+
+        worker.DoWork();
+
+        //loggerMock.Verify(v => v.Log(It.IsAny<string>()), Times.Exactly(5)); // Fail
+
+        loggerMock.Verify(v => v.Log(It.IsAny<string>()), Times.Exactly(10)); // Pass
+    }
+}
